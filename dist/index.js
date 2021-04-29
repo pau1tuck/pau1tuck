009707ddf05c68be8ab63570369c388b79805e2d
Paul Tuck <pau1tuck@outlook.com>
2021-04-30 01:10:40 +0800

    21-04-30T01:10

--- dist/index.js
+++ dist/index.js
@@ -5,9 +5,9 @@ const path_1 = tslib_1.__importDefault(require("path"));
 const express_1 = tslib_1.__importDefault(require("express"));
 const server = () => tslib_1.__awaiter(void 0, void 0, void 0, function* () {
     const app = express_1.default();
-    app.get("/", express_1.default.static(path_1.default.resolve(__dirname, "../public")));
+    app.use(express_1.default.static(path_1.default.resolve(__dirname, "../public")));
     app.listen(8888, () => {
-        console.log("🚀 Node server running on port 6000");
+        console.log("🚀 Node server running on port 8888");
     });
 });
 server().catch((err) => {
--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,10 @@ import express, { Express, Request, Response } from "express";
 const server = async () => {
     const app: Express = express();
 
-    app.get("/", express.static(path.resolve(__dirname, "../public")));
+    app.use(express.static(path.resolve(__dirname, "../public")));
 
     app.listen(8888, () => {
-        console.log("🚀 Node server running on port 6000");
+        console.log("🚀 Node server running on port 8888");
     });
 };
 
